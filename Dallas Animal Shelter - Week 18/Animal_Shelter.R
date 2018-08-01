a1a6d63eaa3b1e40a722fa2043cc37b6a7ba4ca9
Deepal Dsilva <dsilvadeepal@gmail.com>
2018-08-01 18:31:34 -0400

    Updates to TidyTuesday Week 18

--- Dallas Animal Shelter - Week 18/Animal_Shelter.R
+++ Dallas Animal Shelter - Week 18/Animal_Shelter.R
@@ -1,13 +1,27 @@
+setwd("C:/Users/dsilv/Desktop/Learning/Data Science/Tidy-Tuesday-Projects/Dallas Animal Shelter - Week 18")
 
 library(tidyverse)
 library(readxl)
-library(lubridate)
-library(janitor)
-
 
 raw_data <- read_xlsx("week18_dallas_animals.xlsx", sheet = "simple")
 
-str(raw_data)
+#Visualizing Outcomes for all Animals
+
+raw_data %>%
+  group_by(animal_type, outcome_type) %>%
+  summarise(animal_count = n()) %>%
+  ggplot(aes(x = animal_type, y = animal_count, fill = outcome_type)) +
+  geom_bar(stat = "identity", position = "fill", color = "#303030") +
+  labs(title = "Outcome for all Animals",
+       x = "Animal",
+       y = "Outcome") +
+  theme_minimal()
+
+ggsave("Week18_plot1.png")
+
+#####################
+
+#Top Dog Breeds that are Adopted
 
 tidy_data <- raw_data %>%
             filter (outcome_type == "ADOPTION") %>%
@@ -20,13 +34,12 @@ tidy_data <- raw_data %>%
 #To retain the order in the plot
 tidy_data$animal_breed = factor(tidy_data$animal_breed, levels = tidy_data$animal_breed)
 
-glimpse(tidy_data)
 
-#Top Dog Breeds that are Adopted
 tidy_data %>% ggplot(aes(x = animal_breed, y=n)) +
   geom_bar(stat="identity", width = 0.5, fill = "#0e668b") +
-  labs(title="Top 10 Dog Breeds That Are Adopted",
+  labs(title="Top 10 Dog Breed Adoptions",
        y = "Number of Adoptions",
-       x = "")
+       x = "")+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-
+ggsave("Week18_plot2.png")
